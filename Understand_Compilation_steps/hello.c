d49f4cc264d9c3eec9dade1edf1c5b10a27f1ede
shobhit <vermashobhit1994@gmail.com>
2020-09-06 18:25:07 +0530

    added the Understand_Compilation directory

---
+++ Understand_Compilation_steps/hello.c
@@ -0,0 +1,7 @@
+//A simple hello world program to understand the compilation steps of c program
+#include<stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
---
+++ Understand_Compilation_steps/hello.cpp
@@ -0,0 +1,8 @@
+// A hello world program in C++ language 
+#include<iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    cout <<"Welcome to C++ Programming"<<endl;
+}
+
